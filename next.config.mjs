22547bbeccdc1d6106c61ab8c48a5e7456fd681c
Novato111 <91263955+Novato111@users.noreply.github.com>
2025-01-08 23:10:26 +0530

    optimized to handle img data from various domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: [
+      
+//       "scontent.cdninstagram.com",
+//       "instagram.fbom23-1.fna.fbcdn.net", 
+// "scontent-iad3-1.cdninstagram.com"// Add this domain here
+//     ],
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "scontent.cdninstagram.com",
-      "instagram.fbom23-1.fna.fbcdn.net", // Add this domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Accepts all domains
+      },
     ],
   },
 };
