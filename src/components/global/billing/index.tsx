f0592365ab76ff7972882980e8f5a6be553f4c6a
Novato111 <91263955+Novato111@users.noreply.github.com>
2025-01-08 15:04:36 +0530

    sh

--- src/components/global/billing/index.tsx
+++ src/components/global/billing/index.tsx
@@ -7,8 +7,9 @@ const Billing = () => {
   const { data } = useQueryUser();
   return (
     <div className=" flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container ">
-      <PaymentCard current={data?.data?.subscription?.plan} label="PRO" />
-      <PaymentCard current={data?.data?.subscription?.plan} label="FREE" />
+       <PaymentCard current={"PRO"} label="PRO" />
+      <PaymentCard current={"FREE"} label="FREE" />
+      
     </div>
   );
 };
